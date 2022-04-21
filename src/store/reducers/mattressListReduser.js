2317c7999dcdd9826b5165841118ca0a1d13f68b
Nick <101208971+Dogober@users.noreply.github.com>
2022-04-21 23:58:37 +0300

    size-filter

--- src/store/reducers/mattressListReduser.js
+++ src/store/reducers/mattressListReduser.js
@@ -1,18 +1,27 @@
 const initialState = {
-    mattresses: []
+    mattresses: [],
+    filter800x2000: false,
+    filter900x2000: false,
+    filter1800x2000: false,
 }
 
 export const mattressListActionTypes = {
     ADD_MANY_MATTRESSES: 'ADD_MANY_MATTRESSES',
-    FILTER_MATTRESSES_ON_SIZE: 'FILTER_MATTRESSES_ON_SIZE'
+    FILTER_800X2000: 'FILTER_800X2000',
+    FILTER_900X2000: 'FILTER_900X2000',
+    FILTER_1800X2000: 'FILTER_1800X2000'
 }
 
 export const mattressListReducer = (state = initialState, action) => {
     switch (action.type) {
         case mattressListActionTypes.ADD_MANY_MATTRESSES:
-            return {...state, mattresses: [...state.mattresses, ...action.payload]}
-        case mattressListActionTypes.FILTER_MATTRESSES_ON_SIZE:
-            return {...state, mattresses: state.mattresses.filter(mattress => mattress.size === action.payload)}
+            return {...state, mattresses: [...action.payload]}
+        case mattressListActionTypes.FILTER_800X2000:
+            return {...state, mattresses: [], filter800x2000: action.payload}
+        case mattressListActionTypes.FILTER_900X2000:
+            return {...state, mattresses: [], filter900x2000: action.payload}
+        case mattressListActionTypes.FILTER_1800X2000:
+            return {...state, mattresses: [], filter1800x2000: action.payload}            
         default:
             return state;
     }
@@ -20,4 +29,3 @@ export const mattressListReducer = (state = initialState, action) => {
 
 export const addManyMattress = (payload) => ({type: mattressListActionTypes.ADD_MANY_MATTRESSES, payload})
 export const filterMattressesOnSize = (payload) => ({type: mattressListActionTypes.FILTER_MATTRESSES_ON_SIZE, payload})
-
