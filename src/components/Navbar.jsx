f979f7cb34315d1c1a50e543ce56d30d79fd84bb
Nick <101208971+Dogober@users.noreply.github.com>
2022-05-06 12:59:00 +0300

    LOGO

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
                     className='route' 
                     onClick={() => home()}
                 >
-                    Главная
+                    <img className='logo' src='/LOGO3.png'/>
                 </div>
             </div>
             <img 
